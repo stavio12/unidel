10a10dd09456c093a642459774e0e2d372ba7ca2
stavio12 <dzoumedor13@gmail.com>
2021-12-26 09:58:25 +0000

    in the benigin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,3 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-});
-</script>
