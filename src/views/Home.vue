413c5abe9e260a5ceaf6296588779326a6dacd8c
stavio12 <dzoumedor13@gmail.com>
2021-12-27 21:37:26 +0000

    setup Tailwind css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+    <div class="justify-center flex bg-yellow-300 items-center h-screen">  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+    <div class="text-4xl">
+      Hello 👋🏼
+    </div>
+  </div>
 </template>
